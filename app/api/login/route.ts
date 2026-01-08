532560c81aa8d09bc7860091ffa1d3eb35646d3f
dorjoo <dorjoohover@gmail.com>
2026-01-08 10:10:50 +0800

    a

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,27 +1,19 @@
-"use server";
-import { baseUrl } from "@/utils/api";
-import { cookies } from "next/headers";
+"use server";;
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  try {
-    const body = await request.json();
+  const body = await request.json();
 
-    const response = NextResponse.redirect(new URL("/", request.url));
-    console.log(baseUrl)
-    response.cookies.set({
-      name: "token",
-      value: body.token,
-      httpOnly: true,
-      path: "/",
-      sameSite: "lax",
-      secure: false,
-      maxAge: 60 * 60 * 24 * 7,
-    });
-    console.log("login");
-    return response;
-  } catch (error) {
-    console.error("⛔ Route error:", error);
-    return NextResponse.json({ success: false }, { status: 500 });
-  }
+  const res = NextResponse.json({ success: true });
+  res.cookies.set({
+    name: "token",
+    value: body.token,
+    httpOnly: true,
+    path: "/",
+    sameSite: "lax",
+    secure: false,
+    maxAge: 60 * 60 * 24 * 7,
+  });
+
+  return res;
 }
